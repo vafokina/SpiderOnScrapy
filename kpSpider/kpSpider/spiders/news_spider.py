acc1a60e6dc0d2c2d6289f0c42b1cfe6a0e1a9df
Fokina Viktoriya <v.fokina@greendatasoft.ru>
2022-03-02 12:02:32 +0500

    fix: rules for CrawlSpider

--- kpSpider/kpSpider/spiders/news_spider.py
+++ kpSpider/kpSpider/spiders/news_spider.py
@@ -1,27 +1,31 @@
-from turtle import title
 import scrapy
 
+from datetime import datetime
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 class NewsSpider(CrawlSpider):
     name = 'kp_all_news'
+    allowed_domains = ['perm.kp.ru']
     start_urls = ['https://www.perm.kp.ru/']
 
     rules = (
-        Rule(LinkExtractor(allow='daily'), callback='parse_items', follow=True),
+        Rule(LinkExtractor(allow=r".*?/daily/[0-9.]+/\d+"), callback='parse_items', follow=True),
+        Rule(LinkExtractor(allow=()), follow=True),
     )
 
     def parse_items (self, response):
         url = response.url
         title = response.xpath('/html/head/title/text()').get().strip()
         description = response.xpath("//meta[@name='description']/@content")[0].extract().strip()
-        images = response.xpath('//div[@data-gtm-el="content-body"]/*//img/@src').extract()
+        images = response.xpath('//div[@data-gtm-el="content-body"]/*//img/@src').extract() # аналогичное response.css('div[data-gtm-el="content-body"]').css('img::attr(src)').extract()  
+        # publish_date = 
 
         yield{
             'url':url,
             'title':title,
             'description':description,
-            'image_urls':images
+            'image_urls':images,
+            'scraping_date':datetime.now()
         }
         
